0e338f0c644476fbe1aaee7840580597ad0177e5
velka <deepestdark@gmail.com>
2026-05-19 16:53:59 +0900

    feat: add Application class + context load test

---
+++ src/main/java/com/synapse/gateway/SynapseGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.synapse.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapseGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SynapseGatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/synapse/gateway/SynapseGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.synapse.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapseGatewayApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
